8f5822fa41963889a68aa7f2b1e8ec3d45a292ff
Denish <chanddenish@gmail.com>
2023-09-07 00:37:18 +1200

    backend setup_update

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,10 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    });
