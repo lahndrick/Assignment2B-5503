cb9d65894d8bd8b6f59672458f02069407e072a3
Denish <chanddenish@gmail.com>
2023-10-03 03:15:26 +1300

    update backendv2  push

--- backendV2/app.js
+++ backendV2/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
 const app = express();
+
+const port = process.env.PORT || 5000;
 
 // Connect Database
 app.use(express.json())
 
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send(`Hello running on ${port}.`));
 
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
