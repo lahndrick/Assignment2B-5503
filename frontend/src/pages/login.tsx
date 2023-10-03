13f0937f2850819257766650831e894cccb3c8b3
lahndrick <lahndrickhendricks2@gmail.com>
2023-10-03 22:45:15 +1300

    change file to work with vercel

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import formStyles from "../../styles/Form.module.scss";
+import Link from 'next/link';
+import formStyles from '../../styles/Form.module.scss';
 
-export default function login() {
+export default function Login() {
     const formGroupStyle = {
         display: 'flex',
         alignItems: 'center',
@@ -18,21 +18,26 @@ export default function login() {
             <form className={formStyles.form}>
                 <h2>Welcome back!</h2>
                 <div style={formGroupStyle}>
-                    <label htmlFor="username" style={labelStyle}>Username:</label>
+                    <label htmlFor="username" style={labelStyle}>
+                        Username:
+                    </label>
                     <input type="text" id="username" name="username" required />
                 </div>
                 <div style={formGroupStyle}>
-                    <label htmlFor="password" style={labelStyle}>Password:</label>
+                    <label htmlFor="password" style={labelStyle}>
+                        Password:
+                    </label>
                     <input type="password" id="password" name="password" required />
                 </div>
                 <div>
                     <button type="submit">Log in</button>
                 </div>
                 <div>
-                    <a href="/createAccount">Create new Account</a>
+                    <Link href="/createAccount">
+                        <a>Create new Account</a>
+                    </Link>
                 </div>
             </form>
-
         </div>
     );
 }
