e58b02d0ecc21058c4b5ef772549f712bb874ad1
lahndrick <lahndrickhendricks2@gmail.com>
2023-10-03 22:41:12 +1300

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,15 +7,3 @@ export default function Home() {
         </div>
     );
 }
-
-// import React from 'react';
-
-// export default function Home() {
-//   return (
-//     <div className="container">
-//       <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-//       <h2><a href="/login">Log in</a></h2>
-//       <h2><a href="/create-account">Create account</a></h2>
-//     </div>
-//   );
-// }
