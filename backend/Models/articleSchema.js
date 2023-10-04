dde125d8c92bea47a6a82017107ee304fef22062
Denish <chanddenish@gmail.com>
2023-10-05 00:23:15 +1300

    backend local host user creation

---
+++ backend/Models/articleSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+    author: {
+        type: String,
+    },
+    description:{
+        type: String,
+    },
+    title: {
+        type: String,
+    },
+    journal: {
+        type: String,
+    },
+    year: {
+        type: String,
+    },
+    volume: {
+        type: Number,
+    },
+    number: {
+        type: Number,
+    },
+    pages: {
+        type: String,
+    },
+    month: {
+        type: String,
+    },
+    url: {
+        type: String,
+    },
+    DOI:{
+        type: String,
+    },
+    rating:{
+        type: [Number]
+    },
+    semethod:{
+        type: String
+    }
+})
+
+module.exports = articleSchema;
+
--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,43 @@ const app = express()
 const connectDB = require('./connectMongo')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
-
+const bodyParser = require('body-parser');
+// const { hashPassword } = require('./utils');
+const User = require('./Models/userSchema');
 
 // require('dotenv').config()
 
 app.use(express.json())
-
+app.use(cors())
 
 connectDB()
 
 app.get('/', (req, res) =>
-    res.send(`Hello running on ${PORT}. Testing auto deploy`)
-
+    res.send(`Hello running on ${PORT}.`)
 )
+
+
+
+app.post('/createAccount', async (req, res) => {
+    const { username, email, password } = req.body
+ 
+    //create new user
+    const newUser = new User({
+        username,
+        email,
+        password,
+        // password: hashPassword(password)
+    })
+    try {
+        newUser.save()
+        res.status(201).json({message: 'Account created succesfully'});
+    } catch (error) {
+        res.status(409).json({ message: 'Account creation failed' });
+
+    }
+
+})
+
 
 app.listen(PORT, () =>
     console.log(`Server is running on port ${PORT}`)
