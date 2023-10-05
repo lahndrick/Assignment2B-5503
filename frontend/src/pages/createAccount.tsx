69df404f914104f6b4c2a9658ee0d27207e14ecc
Denish Chand <chanddenish@gmail.com>
2023-10-06 02:59:16 +1300

    Update createAccount.tsx

--- frontend/src/pages/createAccount.tsx
+++ frontend/src/pages/createAccount.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import formStyles from "../../styles/login.module.scss";
-import axios from "axios";
 
 export default function CreateAccount() {
     const [formData, setFormData] = useState({
