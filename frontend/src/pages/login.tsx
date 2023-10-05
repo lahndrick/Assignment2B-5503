a9c6754bb6ae46613762e16a6408056458ceabf7
Denish Chand <chanddenish@gmail.com>
2023-10-06 04:20:58 +1300

    Update login.tsx

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -29,7 +29,7 @@ export default function Login() {
         e.preventDefault();
 
         try {
-             const response = await fetch('https://speedbackend.vercel.app', {
+             const response = await fetch('https://speedbackend.vercel.app/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
